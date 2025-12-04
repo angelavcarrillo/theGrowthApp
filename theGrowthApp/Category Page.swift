c93579e19b82521943793b6d8ab1d4c159d386bb
Tim <tsoundavong@gmail.com>
2025-12-04 09:43:20 -0500

    restaging this view so we see on github

---
+++ theGrowthApp/Category Page.swift
@@ -0,0 +1,12 @@
+//
+//  Category Page.swift
+//  theGrowthApp
+//
+//  Created by Timothy Soundavong on 12/4/25.
+//
+
+import SwiftUI
+
+#Preview {
+    
+}
--- theGrowthApp/OnboardingView.swift
+++ theGrowthApp/OnboardingView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            
+            Color.customBeige
+                .ignoresSafeArea() // fills whole screen
+            
+            VStack {
+                Image(.leaf)
+                    .resizable()
+                    .frame(width:500,height:500)
+                    .padding  (.top, -120)
+                
+                Text("Welcome to Growth")
+                    .font(.largeTitle)
+                    .padding(.top, -150)
+                
+                Text("The app designed to grow with you and your goals.")
+                    .font (.system(size: 24))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, -90)
+                    .padding(.horizontal, 60)
+                
+                
+                Text("Select up to 3 goals, complete tasks and watch your tree grow!")
+                    .font (.system(size: 24))
+                    .multilineTextAlignment(.center)
+                    .padding(.top, -20)
+                    .padding(.horizontal, 60)
+                
+                
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+        }
     }
 }
-
+    
 #Preview {
     OnboardingView()
 }
