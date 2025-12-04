ae6bae9dc3d88d01305cf2d130d06b5c5ac81adf
Ali <aliq529@gmail.com>
2025-12-04 09:37:14 -0500

    added ReflectionView() file still working on it

---
+++ theGrowthApp/ReflectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ReflectionView.swift
+//  theGrowthApp
+//
+//  Created by Ali Qasem on 12/4/25.
+//
+
+import SwiftUI
+
+struct ReflectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReflectionView()
+}
