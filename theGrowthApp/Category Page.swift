7e1766b7ccfe2cf795248706960faf9e3f421a16
Tim <tsoundavong@gmail.com>
2025-12-04 11:28:52 -0500

    Added categories in the code, added text and background color

--- theGrowthApp/Category Page.swift
+++ theGrowthApp/Category Page.swift
@@ -6,7 +6,33 @@
 //
 
 import SwiftUI
-
-#Preview {
+struct CategoryPage: View {
     
+    @State private var
+    selectedCategories: [String] = []
+    let categories = ["Finance, Cooking, Fitness, Film, Travel, Art"]
+    
+    var body: some View {
+        
+        ZStack {
+            Color.customBeige.ignoresSafeArea()
+            VStack (spacing: 20) {
+                Text ("Growth")
+                    .font(.system(size: 40,))
+                    .padding(.top, 40)
+                Text ("Which of the following goals align with you?")
+                    .font(.system(size :18))
+                    .multilineTextAlignment(.center)
+                Text ("Select up to 3 categories")
+                    .font(.system(size: 16))
+                    .foregroundColor(.red)
+                    .padding(.bottom, 10)
+            }
+        }
+    
+        
+    }
+}
+    #Preview {
+        CategoryPage()
 }
